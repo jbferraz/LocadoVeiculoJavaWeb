041d9f8c50696730011dd2ad4ba543ae68ff8ef5
felipebkl <felipebkl@gmail.com>
2016-09-25 15:44:38 -0300

    add algumas funcionalidades do usuario

--- LocadoraVeiculoJavaWeb/src/java/bean/LocacaoMB.java
+++ LocadoraVeiculoJavaWeb/src/java/bean/LocacaoMB.java
@@ -6,31 +6,31 @@
 package bean;
 
 import model.Locacao;
+
 import java.util.ArrayList;
 import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 
-
 /**
  *
  * @author JairFerraz
  */
-
 @Named
 @ApplicationScoped
 public class LocacaoMB {
 
     //CRUD
+    
     private List<Locacao> listaLocacoes;
     private Locacao locacaoSelecionado;
 
     public LocacaoMB() {
         locacaoSelecionado = new Locacao();
         listaLocacoes = new ArrayList<Locacao>();
-       // listaVeiculos.add(new Veiculo("admin", "admin"));
+
     }
-    
+
     public Locacao getLocacaoSelecionado() {
         return locacaoSelecionado;
     }
@@ -38,38 +38,39 @@ public class LocacaoMB {
     public void setLocacaoSelecionado(Locacao locacaoSelecionado) {
         this.locacaoSelecionado = locacaoSelecionado;
     }
-
-    public List<Locacao> getListaVeiculos() {
+    
+    public List<Locacao> getListaLocacoes() {
         return listaLocacoes;
     }
-
+    
     public void setListaLocacoes(List<Locacao> listaLocacoes) {
         this.listaLocacoes = listaLocacoes;
     }
     
 
-    public String novoLocacao(){
-        locacaoSelecionado=new Locacao();
-        return("/admin/formularioLocacao?faces-redirect=true");
+    public String novoLocacao() {
+        locacaoSelecionado = new Locacao();
+        return ("/usuario/locacaoVeiculo?faces-redirect=true");
     }
 
-    public String adicionarLocacao()
-    {
+    public String adicionarLocacao() {
+        
         listaLocacoes.add(locacaoSelecionado);
-        return(this.novoLocacao());
+        return (this.novoLocacao());
     }
 
-    public String editarLocacao(Locacao u){
+    public String editarLocacao(Locacao u) {
         locacaoSelecionado = u;
-        return("/admin/edicaoLocacao?faces-redirect=true");
-    }
-    public String atualizarLocacao()
-    {
-        return("/admin/listaLocacoes?faces-redirect=true");
+        return ("/admin/edicaoLocacao?faces-redirect=true");
     }
 
-    public void removerLocacao(Locacao locacao){
+    public String atualizarLocacao() {
+        return ("/usuario/listaLocacoes?faces-redirect=true");
+    }
+
+    public void removerLocacao(Locacao locacao) {
+        
         listaLocacoes.remove(locacao);
+        
     }
-
 }
